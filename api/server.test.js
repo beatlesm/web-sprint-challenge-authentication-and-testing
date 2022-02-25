48d853b96fecd0a7b7648f07e0b7ff2e61dd70c9
beatlesm <rj.geng@gmail.com>
2022-02-25 11:50:24 -0800

    test the sanity & environment - [1] sanity

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+const db = require('../data/dbConfig')
+
+describe('test the sanity & environment', () => {
+  test('[1] sanity', () => {
+    expect(true).toBe(true)
+  })  
+  test.todo('[2] NODE_ENV is correct')
 })
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('GET /api/jokes', () => {
+  it.todo('[3] no token cannot see jokes');
+  it.todo('[4] no valid token can see jokes');
+});
